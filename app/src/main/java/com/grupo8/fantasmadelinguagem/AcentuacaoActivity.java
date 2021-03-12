b18c2b26e3e7a105ca54c4563e3eaad3ae2aa842
FirminoVeras <firmino.veras@live.com>
2021-03-12 07:59:16 -0300

    Menu reformulado

--- app/src/main/java/com/grupo8/fantasmadelinguagem/AcentuacaoActivity.java
+++ app/src/main/java/com/grupo8/fantasmadelinguagem/AcentuacaoActivity.java
@@ -1,11 +1,15 @@
 package com.grupo8.fantasmadelinguagem;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 import android.animation.ValueAnimator;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.media.MediaPlayer;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
@@ -14,7 +18,6 @@ import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
-import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -31,20 +34,24 @@ import java.util.stream.Collectors;
 
 public class AcentuacaoActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     
+    private Timer timer;
+    private String[] roundWords;
     private TextToSpeech mSpeak;
     private int mPoints = 0;
     private int mRound = 0;
     private int mWinPoints = 200;
+    private int[] mRoundPoints = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     
     private TextView mTextWinPoints;
     private TextView mTextRound;
     private TextView mTextDifficult;
     private TextView mResposta;
     private ProgressBar mRoundProgress;
-    Timer timer;
     
-    
-    String[] roundWords;
+    private MediaPlayer mSucessSound;
+    private MediaPlayer mFailSound;
+    private MediaPlayer mBuySound;
+    private MediaPlayer mPassSound;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +63,11 @@ public class AcentuacaoActivity extends AppCompatActivity implements TextToSpeec
         mTextDifficult = findViewById(R.id.Acentuacao_Text_Nivel_Dificuldade);
         mRoundProgress = findViewById(R.id.Acentuacao_Progress_Niveis);
         mResposta = findViewById(R.id.Acentuacao_Text_Resposta);
+    
+        mSucessSound =  MediaPlayer.create(this, R.raw.acentuacao_sucess);
+        mFailSound =  MediaPlayer.create(this, R.raw.acentuacao_fail);
+        mBuySound =  MediaPlayer.create(this, R.raw.acentuacao_buy);
+        mPassSound =  MediaPlayer.create(this, R.raw.acentuacao_pass);
         
         Handler handler = new Handler();
         findViewById(R.id.Acentuacao_Speak_Button).setOnClickListener(v -> {
@@ -67,7 +79,6 @@ public class AcentuacaoActivity extends AppCompatActivity implements TextToSpeec
                 timer.scheduleAtFixedRate(new TimerTask() {
                     @Override
                     public void run() {
-                        
                         handler.post(() -> {
                             if (mWinPoints > 100) {
                                 mWinPoints--;
@@ -78,10 +89,31 @@ public class AcentuacaoActivity extends AppCompatActivity implements TextToSpeec
                     }
                 }, 1000, 1000);
             }
-            v.startAnimation(AnimationUtils.loadAnimation(this,R.anim.acentuacao_button));
-            new Handler().postDelayed(() -> findViewById(R.id.Acentuacao_Speak_Button_Back1).startAnimation(AnimationUtils.loadAnimation(this,R.anim.acentuacao_button)), 50);
-            new Handler().postDelayed(() -> findViewById(R.id.Acentuacao_Speak_Button_Back2).startAnimation(AnimationUtils.loadAnimation(this,R.anim.acentuacao_button)), 120);
+            v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.acentuacao_button));
+            new Handler().postDelayed(() -> findViewById(R.id.Acentuacao_Speak_Button_Back1).startAnimation(AnimationUtils.loadAnimation(this, R.anim.acentuacao_button)), 50);
+            new Handler().postDelayed(() -> findViewById(R.id.Acentuacao_Speak_Button_Back2).startAnimation(AnimationUtils.loadAnimation(this, R.anim.acentuacao_button)), 120);
             new Handler().postDelayed(() -> v.setEnabled(true), 1000);
+        });
+        
+        findViewById(R.id.Acentuacao_Keyboard_Confirm).setOnClickListener(v -> {
+            if (mResposta.getText().toString().length() > 0) {
+                if (roundWords[mRound - 1].equals(mResposta.getText().toString())) {
+                    ((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(500);
+                    mPoints += mWinPoints;
+                    mTextWinPoints.setText(String.valueOf(mPoints));
+                    mRoundPoints[mRound - 1] = mWinPoints;
+                    mSucessSound.start();
+                    startNextRound();
+                } else {
+                    ((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(new long[]{50, 100, 100, 500}, -1);
+                    if (mWinPoints > 100) {
+                        mFailSound.start();
+                        changeWinPoints(-100);
+                    } else {
+                        endGame(false);
+                    }
+                }
+            }
         });
         
         initKeyboard();
@@ -108,6 +140,32 @@ public class AcentuacaoActivity extends AppCompatActivity implements TextToSpeec
                 allHardWord[indexHard.get(1)].toUpperCase(),
                 allHardWord[indexHard.get(2)].toUpperCase(),
                 allVeryHardWords[random.nextInt(allVeryHardWords.length)].toUpperCase()};
+        
+        findViewById(R.id.Acentuacao_Button_Pass).setOnClickListener(v -> {
+            if (mPoints >= 50) {
+                ((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(new long[]{50, 100, 100, 300}, -1);
+                mPassSound.start();
+                mPoints -= 50;
+                mTextWinPoints.setText(String.valueOf(mPoints));
+                startNextRound();
+            }
+        });
+        
+        findViewById(R.id.Acentuacao_Button_Buy_Tip).setOnClickListener(v -> {
+            if (mPoints >= 50) {
+                ((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(new long[]{50, 100, 100, 100}, -1);
+                mBuySound.start();
+                mPoints -= 50;
+                mTextWinPoints.setText(String.valueOf(mPoints));
+                //TODO: colocar dicas
+                View alertContent = getLayoutInflater().inflate(R.layout.dialog_acentuacao_dica, findViewById(R.id.Acentuacao_Dica_Root));
+                ((TextView) alertContent.findViewById(R.id.Acentuacao_Dica_Text)).setText(R.string.lorem);
+                ((TextView) alertContent.findViewById(R.id.Acentuacao_Dica_Referencia)).setText("TESTE, referencia teste, 2021.");
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setView(alertContent);
+                alert.show();
+            }
+        });
         
         startNextRound();
     }
@@ -219,40 +277,6 @@ public class AcentuacaoActivity extends AppCompatActivity implements TextToSpeec
             }
             return true;
         });
-        
-        findViewById(R.id.Acentuacao_Keyboard_Confirm).setOnClickListener(v -> {
-            if (mResposta.getText().toString().length() > 0) {
-                if (roundWords[mRound - 1].equals(mResposta.getText().toString())) {
-                    ((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(1000);
-                    mPoints += mWinPoints;
-                    mTextWinPoints.setText(String.valueOf(mPoints));
-                    startNextRound();
-                } else {
-                    ((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(new long[]{0, 100, 100, 500}, -1);
-                    if (mWinPoints > 100) {
-                        changeWinPoints(-100);
-                    } else {
-                        //TODO:Game Over
-                    }
-                }
-            }
-        });
-        
-        
-        final View.OnTouchListener keyVogalListener = (view, event) -> {
-            view.setAlpha(event.getAction() == MotionEvent.ACTION_DOWN ? 0.5f : 1f);
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                view.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.bg_acentuacao_key, null));
-                view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.acentuacao_key_pop));
-                ((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(50);
-                view.performClick();
-            } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                mResposta.setText(mResposta.getText().toString().concat(((TextView) view).getText().toString()));
-                ((LinearLayout) findViewById(R.id.Acentuacao_Keyboard_Acentos)).removeAllViews();
-            }
-            return true;
-        };
-        
     }
     
     private void startNextRound() {
@@ -260,21 +284,26 @@ public class AcentuacaoActivity extends AppCompatActivity implements TextToSpeec
         timer = null;
         mResposta.setText("");
         mRound++;
-        if (mRound <= 3) {
-            mTextDifficult.setText(R.string.acentuacao_easy_word);
-        } else if (mRound <= 6) {
-            mTextDifficult.setText(R.string.acentuacao_medium_word);
-        } else if (mRound <= 9) {
-            mTextDifficult.setText(R.string.acentuacao_hard_word);
+        if (mRound <= 10) {
+            if (mRound <= 3) {
+                mTextDifficult.setText(R.string.acentuacao_easy_word);
+            } else if (mRound <= 6) {
+                mTextDifficult.setText(R.string.acentuacao_medium_word);
+            } else if (mRound <= 9) {
+                mTextDifficult.setText(R.string.acentuacao_hard_word);
+            } else {
+                mTextDifficult.setText(R.string.acentuacao_veryhard_word);
+            }
+            
+            mWinPoints = 300 + (mRound * 100);
+            
+            mRoundProgress.setProgress(mRound);
+            mTextRound.setText(String.format(Locale.getDefault(), "Nível %d", mRound));
+            ((TextView) findViewById(R.id.Acentuacao_Pontos_Valendo)).setText(String.format(Locale.getDefault(), "%d Pontos", mWinPoints));
         } else {
-            mTextDifficult.setText(R.string.acentuacao_veryhard_word);
+            endGame(true);
         }
         
-        mWinPoints = 300 + (mRound * 100);
-        
-        mRoundProgress.setProgress(mRound);
-        mTextRound.setText(String.format(Locale.getDefault(), "Nível %d", mRound));
-        ((TextView) findViewById(R.id.Acentuacao_Pontos_Valendo)).setText(String.format(Locale.getDefault(), "%d Pontos", mWinPoints));
     }
     
     private void changeWinPoints(int change) {
@@ -288,5 +317,48 @@ public class AcentuacaoActivity extends AppCompatActivity implements TextToSpeec
         anim.addUpdateListener(animation -> findViewById(R.id.Acentuacao_SpreakCount).setAlpha((Float) animation.getAnimatedValue()));
         anim.setDuration(1000);
         anim.start();
+    }
+    
+    private void endGame(boolean win) {
+        View alertContent = getLayoutInflater().inflate(R.layout.dialog_acentuacao_end_game, findViewById(R.id.Acentuacao_End_Root));
+        
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Text)).setText(win ? "Você chegou até o final!" : "Não foi dessa vez!");
+        
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word1)).setText(roundWords[0]);
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word2)).setText(roundWords[1]);
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word3)).setText(roundWords[2]);
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word4)).setText(roundWords[3]);
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word5)).setText(roundWords[4]);
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word6)).setText(roundWords[5]);
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word7)).setText(roundWords[6]);
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word8)).setText(roundWords[7]);
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word9)).setText(roundWords[8]);
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Word10)).setText(roundWords[9]);
+        
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point1)).setText(String.valueOf(mRoundPoints[0]));
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point2)).setText(String.valueOf(mRoundPoints[0]));
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point3)).setText(String.valueOf(mRoundPoints[0]));
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point4)).setText(String.valueOf(mRoundPoints[3]));
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point5)).setText(String.valueOf(mRoundPoints[4]));
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point6)).setText(String.valueOf(mRoundPoints[5]));
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point7)).setText(String.valueOf(mRoundPoints[6]));
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point8)).setText(String.valueOf(mRoundPoints[7]));
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point9)).setText(String.valueOf(mRoundPoints[8]));
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Point10)).setText(String.valueOf(mRoundPoints[9]));
+        
+        ((TextView) alertContent.findViewById(R.id.Acentuacao_End_Points)).setText(String.valueOf(mPoints));
+        
+        //TODO: tratar isso
+        alertContent.findViewById(R.id.Acentuacao_End_Record).setVisibility(View.VISIBLE);
+        
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setView(alertContent);
+        Dialog dialog = alert.show();
+        dialog.setCancelable(false);
+        
+        alertContent.findViewById(R.id.Acentuacao_End_Close).setOnClickListener(v -> {
+            dialog.dismiss();
+            finish();
+        });
     }
 }
--- app/src/main/java/com/grupo8/fantasmadelinguagem/MainActivity.java
+++ app/src/main/java/com/grupo8/fantasmadelinguagem/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.Main_Acentuacao).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AcentuacaoActivity.class)));
+        findViewById(R.id.Splash_Acentuacao).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AcentuacaoActivity.class)));
     }
 }
